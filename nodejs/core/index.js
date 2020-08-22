0326f98956ea304d4ddfb5b5dfeeb5cc4de0e5ae
mdjamilaj <mdjamilaj1@gmail.com>
2020-08-22 14:49:52 +0600

    nodejs server

--- nodejs/core/index.js
+++ nodejs/core/index.js
@@ -1,5 +1,85 @@
-const Person = require('./person')
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const person1 = new Person('Muhammad Jamil', 19);
+const server = http.createServer((req, res) => {
+    // if(req.url === '/'){
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+    //         if(err) throw err;
+    //         res.writeHead(200, {'Content-Type': 'text/html'})
+    //         res.end(content)
+    //     })
+    // }
 
-person1.greeting();
+    // if(req.url === '/about'){
+    //     fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+    //         if(err) throw err;
+    //         res.writeHead(200, {'Content-Type': 'text/html'})
+    //         res.end(content)
+    //     })
+    // }
+
+
+    // if(req.url === '/api/users'){
+    //     const users = [
+    //         {name: "muhammad jamil", age: 19},
+    //         {name: "muhammad aj", age: 14},
+    //     ]
+        
+    //     res.writeHead(200, {'Content-Type': 'application/json'})
+    //     res.end(JSON.stringify(users));
+    // }
+
+
+    // Buiild file path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    
+    //Extension of file 
+    let extname = path.extname();
+
+    //Initail content type
+    let contentType = 'text/html';
+
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case ".css":
+            contentType = "text/css";
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png': 
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+    //Read File
+    fs.readFile(filePath, (err, content) => {
+        if(err){
+            if(err.code == 'ENOENT'){
+                //page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.end(content, 'utf8')
+                })
+            }else{
+                //Some server error
+                res.writeHead(500)
+                res.end(`Server Error: ${err.code}`)
+            }
+        }else{
+            //success
+            res.writeHead(200, {'Content-Type': contentType});
+            res.end(content, 'utf8')
+        }
+    })
+})
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- nodejs/core/logger.js
+++ nodejs/core/logger.js
@@ -1,5 +1,21 @@
 const EventEmitter = require('events');
 const uuid = require('uuid');
 
+class Logger extends EventEmitter {
+    log(msg){
+        //call event
+        this.emit('message', {id: uuid.v4(), msg});
+    }
+}
 
-console.log(uuid.v4());
+module.exports = Logger;
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called Listener:', data))
+
+logger.log("hello world");
+logger.log("hi");
+logger.log("hello");
